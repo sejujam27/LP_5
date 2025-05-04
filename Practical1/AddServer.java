a68016d6c66b664529c654501385e2f2e65de208
Sejal Jam <157806839+sejujam27@users.noreply.github.com>
2025-05-04 23:53:02 +0530

    Add files via upload

---
+++ Practical1/AddServer.java
@@ -0,0 +1,14 @@
+import java.rmi.Naming;
+
+public class AddServer {
+    public static void main(String args[]) {
+        try {
+            AddServerImpl addServerImpl = new AddServerImpl();
+            Naming.rebind("AddServer", addServerImpl);
+            System.out.println("in server side");
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        }
+    }
+}
+
